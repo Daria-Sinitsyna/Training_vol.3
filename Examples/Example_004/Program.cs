d4829933e57e05d78244bce51407fffcc36debf8
Daria <sinitsyna.daria@gmail.com>
2023-02-05 02:37:43 +0300

    Example_004 деление

---
+++ Examples/Example_004/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, la la la World!");
+double numberA = 3;
+double numberB = 4;
+double result = numberA / numberB;
+Console.WriteLine(result);
